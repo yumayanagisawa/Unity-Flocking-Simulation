263df3553a3afbec917d1a3af2bd64b1197cd901
Yuma Yanagisawa <ya7gisa0@gmail.com>
2018-01-10 09:00:50 +0900

    initial commit

---
+++ Flock/Assets/Flock/CustomFlock.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomFlock : MonoBehaviour
+{
+
+    public CustomFlockManager myManager;
+    public float speed;
+    bool turning = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // determine the bounding box of the manager cube
+        Bounds b = new Bounds(myManager.transform.position, myManager.limits * 2);
+
+        // if a typo is outside the boudns of the cube or about to hit something
+        // then start turning around
+        RaycastHit hit = new RaycastHit();
+        Vector3 direction = Vector3.zero;
+
+        if (!b.Contains(transform.position))
+        {
+            turning = true;
+            direction = myManager.transform.position - transform.position;
+        }
+        else if (Physics.Raycast(transform.position, this.transform.forward * 50, out hit))
+        {
+            turning = true;
+            direction = Vector3.Reflect(this.transform.forward, hit.normal);
+        }
+        else
+        {
+            turning = false;
+        }
+
+        if (turning)
+        {
+            // turn towards the centre of the manager cube
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                    Quaternion.LookRotation(direction),
+                                                    myManager.rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            if (Random.Range(0, 100) < 10)
+            {
+                speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
+            }
+            if (Random.Range(0, 100) < 20)
+            {
+                ApplyRules();
+            }
+        }
+
+        transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+
+    void ApplyRules()
+    {
+        GameObject[] gos;
+        gos = myManager.allTypos;
+
+
+        Vector3 vCentre = Vector3.zero;
+        Vector3 vAvoid = Vector3.zero;
+        float gSpeed = 0.01f;
+        float nDistance;
+        int groupSize = 0;
+
+        foreach (GameObject go in gos)
+        {
+            if (go != this.gameObject)
+            {
+                nDistance = Vector3.Distance(go.transform.position, this.transform.position);
+                if (nDistance <= myManager.neighbourDistance)
+                {
+                    vCentre += go.transform.position;
+                    groupSize++;
+
+                    if (nDistance < 1.0f)
+                    {
+                        vAvoid = vAvoid + (this.transform.position - go.transform.position);
+                    }
+
+                    CustomFlock anotherFlock = go.GetComponent<CustomFlock>();
+                    gSpeed = gSpeed + anotherFlock.speed;
+                }
+
+            }
+        }
+
+        if (groupSize > 0)
+        {
+            vCentre = vCentre / groupSize + (myManager.goalPos - this.transform.position);
+            speed = gSpeed / groupSize;
+
+            Vector3 direction = (vCentre + vAvoid) - transform.position;
+            if (direction != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                        Quaternion.LookRotation(direction),
+                                                        myManager.rotationSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Flock/Assets/Flock/CustomFlockManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomFlockManager : MonoBehaviour
+{
+
+    // supposed to be many prefabs
+    public GameObject typoPrefab;
+    public int numTypos = 281;
+    public GameObject[] allTypos;
+    public Vector3 limits = new Vector3(5, 5, 5);
+    public Vector3 goalPos;
+
+    [Header("Typo Settings")]
+    [Range(0.0f, 5.0f)]
+    public float minSpeed;
+    [Range(0.0f, 5.0f)]
+    public float maxSpeed;
+    [Range(1.0f, 10.0f)]
+    public float neighbourDistance;
+    [Range(0.0f, 15.0f)]
+    public float rotationSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        allTypos = new GameObject[numTypos];
+        for (int i = 0; i < numTypos; i++)
+        {
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-limits.x, limits.x),
+                                                                Random.Range(-limits.y, limits.y),
+                                                                Random.Range(-limits.z, limits.z));
+            allTypos[i] = (GameObject)Instantiate(typoPrefab, pos, Quaternion.identity);
+            allTypos[i].GetComponent<CustomFlock>().myManager = this;
+        }
+        goalPos = this.transform.position;
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (Random.Range(0, 100) < 50)
+        {
+            goalPos = this.transform.position + new Vector3(Random.Range(-limits.x, limits.x),
+                                                            Random.Range(-limits.y, limits.y),
+                                                            Random.Range(-limits.z, limits.z));
+        }
+        */
+        goalPos.y = Mathf.Sin(Time.time * 3f) * 0.15f;
+        if (Time.realtimeSinceStartup > 10.0f)
+        {
+            goalPos.y = (Time.realtimeSinceStartup - 10.0f) * (Time.realtimeSinceStartup - 10.0f) * 0.2f + Mathf.Sin(Time.time * 3f) * 0.15f;
+        }
+        this.transform.position = goalPos;
+    }
+    
+}
